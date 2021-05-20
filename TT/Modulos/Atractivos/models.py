0d909aad2a859b52b00a76dafca38a5bb8ec7323
Maxzkp <max.zkp@gmail.com>
2021-05-19 21:05:47 -0500

    Modelo <Atractivo> Completo

--- TT/Modulos/Atractivos/models.py
+++ TT/Modulos/Atractivos/models.py
@@ -1,3 +1,39 @@
 from django.db import models
+from django.db.models.deletion import CASCADE
+from django.db.models.fields import CharField, IntegerField, TimeField
+from django.db.models.fields.related import ForeignKey, ManyToManyField
 
 # Create your models here.
+class Tipo(models.Model):
+    descripcion = CharField(max_length=50)
+
+class Zona(models.Model):
+    descripcion = CharField(max_length=50)
+
+class Categoria(models.Model):
+    descripcion = CharField(max_length=50)
+
+class Servicio(models.Model):
+    descripcion = CharField(max_length=50)
+
+class Atractivo(models.Model):
+    tipo = ForeignKey(Tipo, on_delete=CASCADE)
+    zona = ForeignKey(Zona, on_delete=CASCADE)
+    categorias = ManyToManyField(Categoria)
+    servicios = ManyToManyField(Servicio)
+    nombre = CharField(max_length=100)
+    descripcion = CharField(max_length=600)
+    lat = IntegerField()
+    lon = IntegerField()
+
+class Horario(models.Model):
+    atractivo = ForeignKey(Atractivo, on_delete=CASCADE)
+    inicio = TimeField()
+    final = TimeField()
+    dias = IntegerField()
+    descripcion = CharField(max_length=100)
+
+class Precio(models.Model):
+    atractivo = ForeignKey(Atractivo, on_delete=CASCADE)
+    cantidad = IntegerField()
+    descripcion = CharField(max_length=100)
