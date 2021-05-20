05e9e887c2abb0e87079595872a34a9efd55db0d
Maxzkp <max.zkp@gmail.com>
2021-05-19 21:11:06 -0500

    Modelo <Reseña> Terminado (Revisar clase usuario)

--- TT/Modulos/Reseñas/models.py
+++ TT/Modulos/Reseñas/models.py
@@ -1,3 +1,14 @@
+from django.db.models.enums import Choices
+from django.db.models.fields import CharField
+from Modulos.Atractivos.models import Atractivo
 from django.db import models
-
+from django.db.models.deletion import CASCADE
+from django.db.models.fields.related import ForeignKey
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Reseña(models.Model):
+    usuario = ForeignKey(User, on_delete=CASCADE)
+    atractivo = ForeignKey(Atractivo, on_delete=CASCADE)
+    evaluacion = Choices([1,2,3,4,5])
+    Descripcion = CharField(max_length=600)
