c8a752e26c4ff6fc9b20363c6b69680a2665c1ec
Maxzkp <max.zkp@gmail.com>
2021-06-05 17:00:52 -0500

    Vista administrativa de usuarios terminada

--- TT/Modulos/Usuarios/views.py
+++ TT/Modulos/Usuarios/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib.auth.models import Group, User
 
 # Create your views here.
+def admin_vista(request):
+    if request.method == 'GET':
+        usuarios = User.objects.all()
+        usuarios = usuarios.exclude(groups__name__in=['Superuser', 'Administrador'])
+        return render(request, "admin_usuarios_vista.html", {'usuarios':usuarios})
+    elif request.method == 'POST':
+        usuario = User.objects.filter(username=request.POST['req'])
+        if usuario.first() != None:
+            usuario = usuario.first()
+            usuario.is_active = not usuario.is_active
+            usuario.save()
+        return redirect('/lista_usuarios/') 
+        #render(request, "admin_usuarios_vista.html", {'usuarios':usuarios})
+
+def turista_registrar(request):
+    return render(request, "turista_usuarios_registrar.html")
+
+def turista_recuperarContraseña(request):
+    return render(request, "turista_usuarios_recuperarContraseña.html")
--- TT/TT/urls.py
+++ TT/TT/urls.py
@@ -13,9 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from Modulos.Usuarios.views import admin_vista, turista_recuperarContraseña, turista_registrar
 from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('lista_usuarios/', admin_vista, name='lista_usuarios'),
+    path('registro/', turista_registrar, name='registro'),
+    path('recuperar_contraseña/', turista_recuperarContraseña, name='recuperar_contraseña'),
 ]
